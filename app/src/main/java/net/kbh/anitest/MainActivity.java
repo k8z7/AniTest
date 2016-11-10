2be7b0cb71b09fb1002a701cbc427b220305141c
Your Name <you@example.com>
2016-11-11 03:31:15 +0900

    088) 20161111 아이콘 Frame Animation(연속적으로 이미지 변경하기)

---
+++ app/src/main/java/net/kbh/anitest/MainActivity.java
@@ -0,0 +1,26 @@
+package net.kbh.anitest;
+
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onWindowFocusChanged(boolean hasFocus) {
+        // TODO Auto-generated method stub
+        ImageView imgView = (ImageView) findViewById(R.id.animationImage);
+        imgView.setVisibility(ImageView.VISIBLE);
+        imgView.setBackgroundResource(R.anim.simple_animation);
+        AnimationDrawable frameAnimation = (AnimationDrawable) imgView.getBackground();
+        frameAnimation.start();
+        super.onWindowFocusChanged(hasFocus);
+    }
+
+}
